e24e06273b254d4b8790f2dec20c94abf78e33af
vlp <vlp@fdn.fr>
2026-01-16 14:42:15 +0100

    separate nc

--- configuration.nix
+++ configuration.nix
@@ -9,6 +9,7 @@
       ./services/transmission.nix
       ./services/headscale.nix
       ./services/ttyd.nix
+      ./services/nextcloud.nix
     ];
 
   # Bootloader.
@@ -333,49 +334,49 @@
   };
 
   # Nextcloud conf
-  services.nextcloud = {
-    enable = true;
-    package = pkgs.nextcloud32;
-    hostName = "localhost";
-    database.createLocally = true;
-    configureRedis = true;
-    maxUploadSize = "1G";
-    https = true;
-    autoUpdateApps.enable = true;
-    config = {
-        adminpassFile = config.age.secrets.nextcloud.path;
-        dbtype = "pgsql";
-    };
-    settings = {
-        overwriteProtocol = "https";
-        default_phone_region = "FR";
-        trusted_domains = [ "sandbox.vlp.fdn.fr" "nuage.vlp.fdn.fr"];
-        trusted_proxies = [ "192.168.1.42" ];
-        log_type = "file";
-        memories.exiftool = "${lib.getExe pkgs.exiftool}";
-        enabledPreviewProviders = [
-          "OC\\Preview\\BMP"
-          "OC\\Preview\\GIF"
-          "OC\\Preview\\JPEG"
-          "OC\\Preview\\Krita"
-          "OC\\Preview\\MarkDown"
-          "OC\\Preview\\MP3"
-          "OC\\Preview\\OpenDocument"
-          "OC\\Preview\\PNG"
-          "OC\\Preview\\TXT"
-          "OC\\Preview\\XBitmap"
-          "OC\\Preview\\HEIC"
-       ];
-    };
-    extraApps = {
-      inherit (config.services.nextcloud.package.packages.apps) news bookmarks contacts calendar tasks cookbook notes memories previewgenerator deck;
-    };
-    extraAppsEnable = true;
-    phpOptions."opcache.interned_strings_buffer" = "13";
-    # extra command
-    #nextcloud-occ maintenance:repair --include-expensive
-  };
-  services.nginx.virtualHosts."localhost".listen = [ { addr = "127.0.0.1"; port = 8080; } ];
+  #services.nextcloud = {
+  #  enable = true;
+  #  package = pkgs.nextcloud30;
+  #  hostName = "localhost";
+  #  database.createLocally = true;
+  #  configureRedis = true;
+  #  maxUploadSize = "1G";
+  #  https = true;
+  #  autoUpdateApps.enable = true;
+  #  config = {
+  #      adminpassFile = config.age.secrets.nextcloud.path;
+  #      dbtype = "pgsql";
+  #  };
+  #  settings = {
+  #      overwriteProtocol = "https";
+  #      default_phone_region = "FR";
+  #      trusted_domains = [ "sandbox.vlp.fdn.fr" "nuage.vlp.fdn.fr"];
+  #      trusted_proxies = [ "192.168.1.42" ];
+  #      log_type = "file";
+  #      memories.exiftool = "${lib.getExe pkgs.exiftool}";
+  #      enabledPreviewProviders = [
+  #        "OC\\Preview\\BMP"
+  #        "OC\\Preview\\GIF"
+  #        "OC\\Preview\\JPEG"
+  #        "OC\\Preview\\Krita"
+  #        "OC\\Preview\\MarkDown"
+  #        "OC\\Preview\\MP3"
+  #        "OC\\Preview\\OpenDocument"
+  #        "OC\\Preview\\PNG"
+  #        "OC\\Preview\\TXT"
+  #        "OC\\Preview\\XBitmap"
+  #        "OC\\Preview\\HEIC"
+  #     ];
+  #  };
+  #  extraApps = {
+  #    inherit (config.services.nextcloud.package.packages.apps) news bookmarks contacts calendar tasks cookbook notes memories previewgenerator deck;
+  #  };
+  #  extraAppsEnable = true;
+  #  phpOptions."opcache.interned_strings_buffer" = "13";
+  #  # extra command
+  #  #nextcloud-occ maintenance:repair --include-expensive
+  #};
+  #services.nginx.virtualHosts."localhost".listen = [ { addr = "127.0.0.1"; port = 8080; } ];
 
   systemd.timers."backup_nc" = {
     wantedBy = [ "timers.target" ];
---
+++ services/nextcloud.nix
@@ -0,0 +1,51 @@
+{
+  config,
+  pkgs,
+  ...
+}:
+{
+  services.nextcloud = {                                                                                                                                                                                                                                                      
+    enable = true;                                                                                                                                                                                                                                                            
+    package = pkgs.nextcloud30;                                                                                                                                                                                                                                               
+    hostName = "localhost";                                                                                                                                                                                                                                                   
+    database.createLocally = true;                                                                                                                                                                                                                                            
+    configureRedis = true;                                                                                                                                                                                                                                                    
+    maxUploadSize = "1G";                                                                                                                                                                                                                                                     
+    https = true;                                                                                                                                                                                                                                                             
+    autoUpdateApps.enable = true;                                                                                                                                                                                                                                             
+    config = {                                                                                                                                                                                                                                                                
+        adminpassFile = config.age.secrets.nextcloud.path;                                                                                                                                                                                                                    
+        dbtype = "pgsql";                                                                                                                                                                                                                                                     
+    };                                                                                                                                                                                                                                                                        
+    settings = {                                                                                                                                                                                                                                                              
+        overwriteProtocol = "https";                                                                                                                                                                                                                                          
+        default_phone_region = "FR";                                                                                                                                                                                                                                          
+        trusted_domains = [ "sandbox.vlp.fdn.fr" "nuage.vlp.fdn.fr"];                                                                                                                                                                                                         
+        trusted_proxies = [ "192.168.1.42" ];                                                                                                                                                                                                                                 
+        log_type = "file";                                                                                                                                                                                                                                                    
+        memories.exiftool = "${lib.getExe pkgs.exiftool}";                                                                                                                                                                                                                    
+        enabledPreviewProviders = [                                                                                                                                                                                                                                           
+          "OC\\Preview\\BMP"                                                                                                                                                                                                                                                  
+          "OC\\Preview\\GIF"                                                                                                                                                                                                                                                  
+          "OC\\Preview\\JPEG"                                                                                                                                                                                                                                                 
+          "OC\\Preview\\Krita"                                                                                                                                                                                                                                                
+          "OC\\Preview\\MarkDown"                                                                                                                                                                                                                                             
+          "OC\\Preview\\MP3"   
+          "OC\\Preview\\OpenDocument"
+          "OC\\Preview\\PNG"
+          "OC\\Preview\\TXT"
+          "OC\\Preview\\XBitmap"
+          "OC\\Preview\\HEIC"
+       ];
+    };
+    extraApps = {
+      inherit (config.services.nextcloud.package.packages.apps) news bookmarks contacts calendar tasks cookbook notes memories previewgenerator deck;
+    };
+    extraAppsEnable = true;
+    phpOptions."opcache.interned_strings_buffer" = "13";
+    # extra command
+    #nextcloud-occ maintenance:repair --include-expensive
+  };
+  services.nginx.virtualHosts."localhost".listen = [ { addr = "127.0.0.1"; port = 8080; } ];
+
+}
