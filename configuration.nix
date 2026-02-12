c3a7e326983cdd458a22ae56b09537e4b94718f7
vlp <vlp@fdn.fr>
2026-02-12 14:56:57 +0100

    move nfs

--- configuration.nix
+++ configuration.nix
@@ -14,6 +14,7 @@
       ./services/ttyd.nix
       ./services/nextcloud.nix
       ./services/prom.nix
+      ./services/nfs-mounts.nix
     ];
 
   # Bootloader.
@@ -173,67 +174,6 @@
   services.openvpn.servers = {
     officeVPN = { config = '' config /root/fdn.conf ''; };
   };
-
-  # NAS folder mounting
-  systemd.tmpfiles.rules = [
-    "d /mnt/animations 0751 vlp vlp - -"
-    "d /mnt/audio 0751 vlp vlp - -"
-    "d /mnt/docu 0755 vlp vlp - -"
-    "d /mnt/ebooks 0755 vlp vlp - -"
-    "d /mnt/games 0755 vlp vlp - -"
-    "d /mnt/movies 0755 vlp vlp - -"
-    "d /mnt/tvshows 0755 vlp vlp - -"
-    "d /mnt/downloads 0775 vlp vlp - -"
-    "d /root/backup 0750 root root - -"
-    "d /home/vlp/partages 0750 vlp vlp - -"
-  ];
-
-  fileSystems."/mnt/animations" = {
-    device = "192.168.1.10:/data/animations";
-    fsType = "nfs";
-    options = [ "x-systemd.automount" "noauto" "x-systemd.idle-timeout=600" ];
-  };
-  fileSystems."/mnt/docu" = {
-    device = "192.168.1.10:/data/docu";
-    fsType = "nfs";
-    options = [ "x-systemd.automount" "noauto" "x-systemd.idle-timeout=600" ];
-  };
-  fileSystems."/mnt/ebooks" = {
-    device = "192.168.1.10:/data/ebooks";
-    fsType = "nfs";
-    options = [ "x-systemd.automount" "noauto" "x-systemd.idle-timeout=600" ];
-  };
-  fileSystems."/mnt/games" = {
-    device = "192.168.1.10:/data/games";
-    fsType = "nfs";
-    options = [ "x-systemd.automount" "noauto" "x-systemd.idle-timeout=600" ];
-  };
-  fileSystems."/mnt/movies" = {
-    device = "192.168.1.10:/data/movies";
-    fsType = "nfs";
-    options = [ "x-systemd.automount" "noauto" "x-systemd.idle-timeout=600" ];
-  };
-  fileSystems."/mnt/tvshows" = {
-    device = "192.168.1.10:/data/tvshows";
-    fsType = "nfs";
-    options = [ "x-systemd.automount" "noauto" "x-systemd.idle-timeout=600" ];
-  };
-  fileSystems."/mnt/audio" = {
-    device = "192.168.1.10:/data/audio";
-    fsType = "nfs";
-    options = [ "x-systemd.automount" "noauto" "x-systemd.idle-timeout=600" ];
-  };
-  fileSystems."/home/vlp/partages" = {
-    device = "192.168.1.10:/data/partages";
-    fsType = "nfs";
-    options = [ "x-systemd.automount" "noauto" "x-systemd.idle-timeout=600" ];
-  };
-  fileSystems."/var/lib/nextcloud/data" = {
-    device = "192.168.1.10:/data/nextcloud";
-    fsType = "nfs";
-    options = [ "x-systemd.automount" "noauto" "x-systemd.idle-timeout=600" ];
-  };
-
 
   # Incus configuration
   virtualisation.incus.enable = true;
