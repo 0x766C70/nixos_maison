fa23a67f19afeacf3cbf65a134f24783d99f28f1
vlp <vlp@fdn.fr>
2026-02-13 17:04:23 +0100

    add update conf

--- configuration.nix
+++ configuration.nix
@@ -4,6 +4,7 @@
   imports =
     [
       ./hardware-configuration.nix
+      ./apps.nix
       ./services/firewall.nix
       ./services/vim.nix
       ./services/msmtp.nix
@@ -72,91 +73,9 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # Flakes setuo
+  # Flakes setup
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  # Package definition
-  environment.systemPackages = with pkgs; [
-
-    # basic tools
-    vim
-    neovim
-    git
-    nnn
-    zip
-    xz
-    unzip
-    p7zip
-    ripgrep
-    jq
-    yq-go
-    eza
-    fzf
-    incus
-
-    # networking tools
-    openvpn
-    lynx
-    mtr
-    iperf3
-    dnsutils
-    ldns
-    aria2
-    socat
-    nmap
-    ipcalc
-    curl
-    wget
-
-    # misc
-    file
-    which
-    tree
-    gnused
-    gnutar
-    gawk
-    zstd
-    gnupg
-    pass
-
-    # productivity
-    glow
-
-    # monitoring
-    btop
-    iotop
-    iftop
-
-    # system call monitoring
-    strace
-    ltrace
-    lsof
-
-    # system tools
-    sysstat
-    lm_sensors
-    ethtool
-    pciutils
-    usbutils
-    nfs-utils
-    go
-    epsonscan2
-    ncdu
-    speedtest-go
-
-    # NC dependencies
-    exiftool
-    ffmpeg
-    imagemagick
-    nodejs
-    perl
-
-    # apps
-    caddy
-    ttyd
-    minidlna
-    inotify-tools
-  ];
   hardware.sane.extraBackends = [ pkgs.epkowa ];
 
   # Service configurations
