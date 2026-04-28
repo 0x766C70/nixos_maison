4ef9d3e7e3e84d86b7e4486daff5b600d6a1c203
vlp <vlp@fdn.fr>
2026-04-28 17:33:52 +0200

    check ageing

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,11 +1,10 @@
 let
   user1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBPQ5fvwLItahdBiHWbQOm7J97PzlZ5QweNk3/m0Weu+ root@maison";
-  user2 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMeZmfJWJaN1lgBWOvD2l/pOQLpNvTflIotJnGeyND+d vlp@maison";
 in
 {
-  "nextcloud.age".publicKeys = [ user1 user2 ];
-  "prom.age".publicKeys = [ user1 user2 ];
-  "mail_infomaniak.age".publicKeys = [ user1 user2];
+  "nextcloud.age".publicKeys = [ user1 ];
+  "prom.age".publicKeys = [ user1 ];
+  "mail_infomaniak.age".publicKeys = [ user1 ];
   "luks_sdb1.age".publicKeys = [ user1 ];
   "luks_sda1.age".publicKeys = [ user1 ];
 }
