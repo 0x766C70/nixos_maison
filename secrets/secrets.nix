c6d660f8e3a5775351730d880917a602db0aa9a7
vlp <vlp@fdn.fr>
2026-04-28 17:50:41 +0200

    rekey

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,10 +1,11 @@
 let
   user1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBPQ5fvwLItahdBiHWbQOm7J97PzlZ5QweNk3/m0Weu+ root@maison";
+  user2 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID8tl4ACfbuY+gY33fBKAu/V9UbXZVXIYSdHDNRLOjQv";
 in
 {
   "nextcloud.age".publicKeys = [ user1 ];
   "prom.age".publicKeys = [ user1 ];
-  "mail_infomaniak.age".publicKeys = [ user1 ];
+  "mail_infomaniak.age".publicKeys = [ user1 user2];
   "luks_sdb1.age".publicKeys = [ user1 ];
   "luks_sda1.age".publicKeys = [ user1 ];
 }
