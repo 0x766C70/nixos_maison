0bd9297baeb065664c4e0332030c669fe96dc5d5
vlp <vlp@fdn.fr>
2026-04-28 18:17:10 +0200

    mail infomaniak ok

--- services/msmtp.nix
+++ services/msmtp.nix
@@ -8,11 +8,10 @@
     accounts.default = {
       auth = "on";
       tls = "on";
-      logfile = "/var/log/msmtp.log";
       host = "mail.infomaniak.com";
       from = "monitoring@766c70.com";
       user = "monitoring@766c70.com";
-      passwordeval = "$(cat ${config.age.secrets.mail_infomaniak.path})";
+      passwordeval = "cat ${config.age.secrets.mail_infomaniak.path}";
     };
   };
 }
